654a6ecdc1c4595f7f383f9364f3e5a43b39f645
Dominic Child <dominicchild@Dominics-MacBook-Pro.local>
2025-08-16 21:39:34 +0100

    Updates

--- src/main/java/com/domain/cardgame/cards/impl/Fireball.java
+++ src/main/java/com/domain/cardgame/cards/impl/Fireball.java
@@ -1,17 +1,27 @@
 package src.main.java.com.domain.cardgame.cards.impl;
+import src.main.java.com.domain.cardgame.cards.CardInterface;
 
-import src.main.java.com.domain.cardgame.cards.AbstractCard;
-
-public class Fireball extends AbstractCard {
+public class Fireball implements CardInterface{
+    private String name;
+    private int manaCost;
     private int damage;
 
     public Fireball(){
-        super("Fireball",1);
+        this.name = "Fireball";
+        this.manaCost = 1;
         this.damage = 30;
     }
 
     @Override
     public void play(){
         System.out.println("Uses Fireball spell");
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getManaCost(){
+        return manaCost;
     }
 }
